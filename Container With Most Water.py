ead3ba98f3740aa284c4a3c7f55ab3f51bbe8cf5
hecter_wu <hecter_wu@transcend.com.tw>
2017-09-15 17:25:29 +0800

    pass testcase: testCase1 by using for loop

--- Container With Most Water.py
+++ Container With Most Water.py
@@ -5,12 +5,20 @@ class unitest(unittest.TestCase):
         h = [1]
         ans = 0
         self.assertEqual(Solution().maxArea(h),ans);
-
+    def testCase1(self):
+        h = [5,8,1,2,3,4,5,6]
+        ans = 36
+        self.assertEqual(Solution().maxArea(h),ans);
 class Solution():
     def maxArea(self, height):
         if len(height) < 2:
             return 0
-        return None
+        Ans = 0
+        for i in range(len(height)):
+            for j in range(i+1,len(height)):
+                tmp = min(height[i],height[j]) * (j-i)
+                Ans = max(Ans,tmp)
+        return Ans
 
 if __name__ == '__main__':
     unittest.main()
